97943f5c28fa1febecece30d7384563493d2579d
Davi Santos <daviantoniosantos@outlook.com>
2024-10-07 15:48:45 -0300

    .

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ let qtd_exp = 6327;
 calculaNivel = (xp) => {
     if (xp < 1000) {
         return "Ferro";
-    } else if (xp >= 1001 && XP <= 2000) {
+    } else if (xp >= 1001 && xp <= 2000) {
         return "Bronze";
-    } else if (xp >= 2001 && XP <= 5000) {
+    } else if (xp >= 2001 && xp <= 5000) {
         return "Prata";
-    } else if (xp >= 5001 && XP <= 7000) {
+    } else if (xp >= 5001 && xp <= 7000) {
         return "Ouro";
-    } else if (xp >= 7001 && XP <= 8000) {
+    } else if (xp >= 7001 && xp <= 8000) {
         return "Platina";
-    } else if (xp >= 8001 && XP <= 9000) {
+    } else if (xp >= 8001 && xp <= 9000) {
         return "Ascendente";
-    } else if (xp >= 9001 && XP <= 10000) {
+    } else if (xp >= 9001 && xp <= 10000) {
         return "Imortal";
     } else if (xp >= 10001) {
         return "Radiante";
